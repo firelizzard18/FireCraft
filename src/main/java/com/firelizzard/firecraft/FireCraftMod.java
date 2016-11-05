2252cf811757dec1cd314a7f7b0e5944b8620274
Ethan Reesor <ethan.reesor@gmail.com>
2016-11-05 00:27:58 -0700

    Version bump

--- src/main/java/com/firelizzard/firecraft/FireCraftMod.java
+++ src/main/java/com/firelizzard/firecraft/FireCraftMod.java
@@ -16,7 +16,7 @@ import net.minecraft.item.Item;
 @Mod(modid = FireCraftMod.MODID, version = FireCraftMod.VERSION)
 public class FireCraftMod extends BaseMod {
 	public static final String MODID = "firecraft";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	
 	public static final int RENDERERID_1 = 1;
 	
